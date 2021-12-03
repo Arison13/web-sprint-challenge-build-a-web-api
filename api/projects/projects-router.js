567896e45eec3bc17c23d5c295aabf80ac68c627
Arison13 <arisonarias@gmail.com>
2021-12-03 13:40:25 -0500

    working on projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -14,7 +14,22 @@ router.get("/", (req, res) => {
 
 router.get("/:id", validateProjectId, (req, res) => {
     res.status(200).json(req.project);
-  });
+});
 
+router.post("/", validateProjectBody, (req, res, next) => {
+    Projects.insert(req.body)
+      .then((newProject) => {
+        res.status(201).json(newProject);
+      })
+      .catch(next);
+});
 
+router.put("/:id",validateProjectId,validateProjectBody,(req, res, next) => {
+      Projects.update(req.params.id, req.body)
+        .then((updatedPost) => {
+          res.status(202).json(updatedPost);
+        })
+        .catch(next);
+    }
+  );
 module.exports = router
