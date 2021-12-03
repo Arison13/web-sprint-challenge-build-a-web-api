68a761f41e306e5f9696cf9aa13522d98bd1d7b9
Arison13 <arisonarias@gmail.com>
2021-12-03 16:45:01 -0500

    finished route for actions all test passing

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,7 @@
 // Write your "actions" router here!
 const express = require('express');
 const Action = require('./actions-model');
-
+const { checkActionBody, checkId} = require('./actions-middlware');
 const router = express.Router();
 
 router.get('/', (req, res, next)=> {
@@ -12,5 +12,34 @@ router.get('/', (req, res, next)=> {
     .catch(next)
 })
 
+router.get("/:id", checkId, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+router.post("/", checkId, checkActionBody, (req, res, next) => {
+      Action.insert(req.body)
+        .then((newAction) => {
+          res.status(201).json(newAction);
+        })
+        .catch(next);
+    }
+);
+
+router.put("/:id", checkId, checkActionBody, (req, res, next) => {
+      Action.update(req.params.id, req.body)
+        .then((updatedAction) => {
+          res.status(202).json(updatedAction);
+        })
+        .catch(next);
+    }
+);
+
+router.delete("/:id", checkId,(req, res, next) => {
+    Action.remove(req.params.id)
+      .then(() => {
+        res.status(204).json({});
+      })
+      .catch(next);
+});
 
 module.exports = router
